f65c819f7c86ff8c3387814f12b9bcd7ebe9aa75
Luong Thanh Tung <tung.luongthanh2004@hcmut.edu.vn>
2025-06-13 16:35:23 +0700

    Initial commit - User registration Web App using ASP.NET Core

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WebFormRegister.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
